0046ee7c1ff826c5133e6467c5a5b7c49dfedfda
PourhajiDeV <aghacherik@gmail.com>
2026-08-14 15:47:00 +0330

    Fix Syntaxs

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>آورا واچ | Aura Watches</title>
+    <title>آورا واچ | Aura Watche</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
